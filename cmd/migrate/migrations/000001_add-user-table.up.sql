3937e92c07e3c181985aa4c7519f44f9e519e272
Asif-Faizal <moh.asif@protonmail.ch>
2025-06-13 17:30:30 +0530

    feature(migrations): created migration sql scripts for user table

---
+++ cmd/migrate/migrations/000001_add-user-table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
