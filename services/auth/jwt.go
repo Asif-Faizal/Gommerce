73b65f52cc017e07c5257825f2d978cae443210f
Asif-Faizal <moh.asif@protonmail.ch>
2025-06-14 18:29:17 +0530

    feature(token): create jwt function implemented

---
+++ services/auth/jwt.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func CreateJWT(secret []byte, userId int) (string, error) {
+	expiration := time.Second * 60 * 60 * 24 // 15 days
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId":    strconv.Itoa(userId),
+		"expiredAt": time.Now().Add(expiration).Unix(),
+	})
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
